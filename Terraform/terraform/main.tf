dd4612461ad3fca6dcf98803038b51e8a0be3b95
RameshKrishnanNaraKrish <nkrk1996@outlook.com>
2024-09-13 22:07:22 -0400

    main remove provisioner

--- Terraform/terraform/main.tf
+++ Terraform/terraform/main.tf
@@ -12,18 +12,6 @@ resource "aws_instance" "example" {
   key_name                    = "mykey"
   associate_public_ip_address = "true"
 
- provisioner "file" {
-    source      = "C:/Users/nkrk1/Documents/GitHub/pythonterraformautomation/Terraform/terraform/jenkinsinstall.sh"
-    destination = "/home/ubuntu/jenkinsinstall.sh"
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("C:/Users/nkrk1/Desktop/Devops HW/New folder/mykey.pem")
-      host        = self.public_ip
-    }
-  }
-
   tags = {
     Name = "terrafomr - ${random_string.name.result}"
   }
