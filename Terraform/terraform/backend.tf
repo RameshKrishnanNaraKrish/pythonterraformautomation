13b96e19dea83832c02d615d98e9518700f06b05
RameshKrishnanNaraKrish <nkrk1996@outlook.com>
2024-09-14 22:48:39 -0400

    update

--- Terraform/terraform/backend.tf
+++ Terraform/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-test-bucket-backends3"
     key            = "ec2/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock-table"
   }
