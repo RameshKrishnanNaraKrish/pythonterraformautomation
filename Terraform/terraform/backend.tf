9eab4202484676a65df52caa9d9f0bdd5a6f7530
RameshKrishnanNaraKrish <nkrk1996@outlook.com>
2024-09-14 22:52:37 -0400

    dynamo db

--- Terraform/terraform/backend.tf
+++ Terraform/terraform/backend.tf
@@ -7,3 +7,14 @@ terraform {
     dynamodb_table = "terraform-lock-table"
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
